eec57346f556bcc38907c86e960b1fc542c7fe73
Boris Demin <demin.b.d@gmail.com>
2025-01-15 00:36:28 +0500

    add: added CustomException interface extends of Exception

---
+++ src/main/java/com/rtfmyoumust/currencyexchange/customexceptions/CustomException.java
@@ -0,0 +1,14 @@
+package com.rtfmyoumust.currencyexchange.customexceptions;
+
+public abstract class CustomException extends Exception {
+
+    public CustomException(String message) {
+        super(message);
+    }
+
+    public CustomException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public abstract int getErrorCode();
+}
