58ff59765c962f0ba838182b79e0b2bcd83a65f0
Boris Demin <demin.b.d@gmail.com>
2025-02-03 15:56:54 +0500

    fix: added check currency code.

--- src/main/java/com/rtfmyoumust/currencyexchange/controllers/CurrencyServlet.java
+++ src/main/java/com/rtfmyoumust/currencyexchange/controllers/CurrencyServlet.java
@@ -1,6 +1,7 @@
 package com.rtfmyoumust.currencyexchange.controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.rtfmyoumust.currencyexchange.customexceptions.FieldNotFoundException;
 import com.rtfmyoumust.currencyexchange.dto.CurrencyResponseDto;
 import com.rtfmyoumust.currencyexchange.service.CurrencyService;
 import jakarta.servlet.annotation.WebServlet;
@@ -18,6 +19,9 @@ public class CurrencyServlet extends HttpServlet {
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String code = request.getPathInfo().substring(1);
+        if (code.isEmpty()){
+            throw new FieldNotFoundException("Код валюты отсутствует в адресе");
+        }
         CurrencyResponseDto currencyDTO = currencyService.getCurrencyByCode(code);
         objectMapper.writeValue(response.getWriter(), currencyDTO);
     }
